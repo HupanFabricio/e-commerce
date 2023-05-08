e870deef71d04a686cdf66b4ce2e1f842f89c066
HupanFabricio <fabrihupan@gmail.com>
2023-05-08 19:57:54 -0300

    Elaboracion de rutas

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,8 @@
+import clientPromise from '@/lib/mongodb'
+import { MongoDBAdapter } from '@next-auth/mongodb-adapter'
 import NextAuth from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
+
 
 export default NextAuth({
   providers: [
@@ -7,5 +10,6 @@ export default NextAuth({
       clientId: process.env.GOOGLE_ID,                                                                                                 
       clientSecret: process.env.GOOGLE_SECRET
     }),
-  ]
+  ],
+  adapter: MongoDBAdapter (clientPromise)
 })
--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,22 @@
-import { useSession, signIn, signOut } from "next-auth/react"
+import Layout from "@/components/Layout";
+import { useSession } from "next-auth/react";
 
 export default function Home() {
-  const { data: session } = useSession()
-  if(!session) {
-    return (
-        <div className='bg-blue-900 w-screen h-screen flex items-center'>
-            <div className="text-center w-full">
-                <buttom onClick={ () => signIn('google') } className='bg-white p-2 px-4 rounded-lg'>Login with Google</buttom> 
-            </div>
-      </div>
-    );
-  }
 
-  return ( 
-    <div>logged in {session.user.email} </div>
-  );
+  const { data: session } = useSession();
+
+  return <Layout>
+    <div className="text-blue-900 flex justify-between">
+      <h2>
+        Hello, <b> {session?.user?.name} </b>
+      </h2>
+      <div className="flex bg-gray-300 gap-1 text-black rounded-lg overflow-hidden">
+        <img src={session?.user?.image} alt="" className="w-6 h-6" />
+        <span className="px-2">
+          {session?.user?.name}
+        </span>
+      </div>
+    </div>
+  </Layout>
 }
 
-// export default function Component() {
-//     const { data: session } = useSession()
-//     if(session) {
-//         return <>
-//             Signed in as {session.user.email} <br/>
-//             <button onClick={() => signOut()}>Sign out</button>
-//         </>
-//   }
-//   return <>
-//     Not signed in <br/>
-//     <button onClick={() => signIn()}>Sign in</button>
-//   </>
-// }
